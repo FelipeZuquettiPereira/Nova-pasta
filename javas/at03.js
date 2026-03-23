5eba586640cb2741b6678323c2947836592d9415
FelipeZuquetti <felipe_z_pereira@estudante.sesisenai.org.br>
2026-03-23 12:57:59 -0300

    questão 3 em andamento

---
+++ javas/at03.js
@@ -0,0 +1,13 @@
+function BalancoFinanceiro(){
+    let Gastos;
+    let Lucro;
+    let meses = new Array(12).fill(null);
+    let Saldo;
+
+   for(let i = 1; i < 13; i++){
+        let meses[i] = parseFloat(prompt("Digite o ganho total do " + i +"º mês:"));
+        
+
+    }
+
+}
